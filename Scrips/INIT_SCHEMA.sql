eac494bcf7fe92c16290e906b7ab7b63c186741c
mohamed talaat <101501733+mohamedaymntalat@users.noreply.github.com>
2025-12-15 17:44:36 +0300

    Update and rename ddl_bronze_layer.sql to INIT_SCHEMA.sql

---
+++ Scrips/INIT_SCHEMA.sql
@@ -0,0 +1,19 @@
+/*
+This code create a datawarehouse and schemas
+you must check if the database is already exist or not
+*/
+
+--This create a database
+CREATE DATABASE Datawarehouse;
+
+--This USING a Datawarehouse database
+use Datawarehouse;
+
+GO  --let sql to excute each line sperately
+--WE create THREE  SCHEMA 
+
+CREATE SCHEMA bronze ;
+Go
+CREATE SCHEMA silver ;
+GO
+CREATE SCHEMA gold ;
